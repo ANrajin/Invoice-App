55059e35e0073434cb23aed65686a39767feca0c
anrajin <an.rajin@gmail.com>
2025-11-20 00:04:18 +0600

    flowbite configured

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -4,9 +4,12 @@ module.exports = {
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "../../node_modules/flowbite-react/dist/**/*.js",
     ],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        require("flowbite/plugin")
+    ],
 }
